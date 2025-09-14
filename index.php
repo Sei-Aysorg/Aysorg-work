432d9242e13e03aa7cad1e11215a7b75db06b16f
Sei-Aysorg <seignon.aysorg@gmail.com>
2025-09-14 12:09:38 +0200

    Ceci est mon premier dépot

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Ceci est un test</title>
+</head>
+<body>
+	<center> 
+
+
+	<h1>Je prépare un visionning avce Git</h1>
+
+
+	</center>
+
+</body>
+</html>
